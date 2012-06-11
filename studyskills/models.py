480aec93d9f919da76d0f8ace859842103150990
Julie Sparks <julie.sparks@cpcc.edu>
2012-06-11 13:02:53 -0400

    Added model and associated south migration script

--- studyskills/models.py
+++ studyskills/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class StudySkillsResult(models.Model):
+    """
+        Stores students reponses to study skills assessment.
+    """
+    student = models.ForeignKey(User)
+    answers = models.TextField()
+    date_taken = models.DateTimeField(auto_now_add=True)
