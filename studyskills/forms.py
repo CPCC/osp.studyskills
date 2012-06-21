3cfdebb7e57895520cbaa8af0782f4f2bf217f63
Julie Sparks <julie.sparks@cpcc.edu>
2012-06-21 11:54:52 -0400

    Added scoring for assessment in result view (updated form to include scoring categories and subcategories); initiated template for displaying assessment result

--- studyskills/forms.py
+++ studyskills/forms.py
@@ -3,94 +3,101 @@ from django.utils import simplejson as json
 
 
 class StudySkillsForm(forms.Form):
+    """
+        This form provides the questions to be asked on the assessment. Each field is
+        also going to have two additional attributes, category and subcategory, that
+        will pertain to how the responses to the questions need to be grouped for scoring
+        purposes. Each inner list will be of the format [question, category, subcategory].
+        To see what the letter codes mean, look in the study_skills_get_result view at
+        the compiled scores dictionary.
+    """
     def __init__(self, *args, **kwargs):
         super(StudySkillsForm, self).__init__(*args, **kwargs)
         
         questions = [
-        'I manage to find conditions for studying which allow me to get on with my work easily.',
-        "When working on an assessment, I am keeping in mind how best to inpress the Instructor.",
-        'Often I find myself wondering whether the work I am doing here is really worthwhile.',
-        'I usually set out to understand for myself the meaning of what we have to learn.',
-        'I organize my study time carefully to make the best use of it.',
-        'I find i have to concentrate on just memorizing a good deal of what we have to learn.',
-        'I go over the work I have done carefullyto check the reasoning and that it makes sense.',
-        'Often I feel I am drowning in the sheer amount of material we are having to cope with.',
-        "I look at the evidence carefully and try to reach my own conclusion about what I am studying.",
-        "It is important for me to feel that I am doing as well as I really can on the courses here.",
-        "I try to relate ideas I come across to those in other topics or other courses whenever possible.",
-        "I tend to read very little beyond what is actually required to pass.",
-        "Regularly I find myself thinking about ideas from lectures when I am doing other things.",
-        "I think I am quite systematic and organised when it comes to revising for exams.",
-        "I look carefully at tutors comments on course work to see how to get higher marks next time.",
-        "There is not much of the work here that I find interesting or relevant.",
-        "When I read an article or book, I try to find out for myself exactly what the author means.",
-        "I am pretty good at getting down to work whenever I need to.",
-        "Much of what I am studying makes little sense: it is like unrelated bits and pieces.",
-        "I think about what I want to get out of this course to keep my studying well focused.",
-        "When I am working on a new topic, I try to see in my own mind how all the ideas fit together.",
-        "I often worry about whether I will ever be able to cope with the work properly.",
-        "Often I find myself questioning things I hear in lectures or read in books.",
-        "I feel that I am getting on well, and this helps me put more effort into the work.",
-        "I concentrate on learning just those bits of information I have to know to pass.",
-        "I find that studying academic topics can be quite exciting at times.",
-        "I am good at following up some of the reading suggested by lecturers or tutors.",
-        "I keep in mind who is going to mark an assignment and what they are likely to be looking for.",
-        "When I look back, I sometimes wonder why I ever decided to come here.",
-        "When I am reading, I stop from time to time to reflect on what I am trying to learn from it.",
-        "I work steadily through the term or semester, rather than leave it all until the last minute.",
-        "I am not really sure what is important in lectures so I try to get down all I can.",
-        "Ideas in course books or articles often set me off on long chains of thought of my own.",
-        "Before starting work on an assignment or exam question, I think first how best to tackle it.",
-        "I often seem to panic if I get behind with my work.",
-        "When I read, I examine the details carefully to see how they fit in with what is being said.",
-        "I put a lot of effort into studying because I am determined to do well.",
-        "I gear my studying closely to just what seems to be required for assignments and exams.",
-        "Some of the ideas I come across on the course I find really gripping.",
-        "I usually plan out my weeks work in advance, either on paper or in my head.",
-        "I keep an eye open for what lecturers seem to think is important and concentrate on that.",
-        "I am not really interested in this course, but I have to take it for other reasons.",
-        "Before tackling a problem or assignment, I first try to work out what lies behind it.",
-        "I generally make good use of my time during the day.",
-        "I often have trouble in making sense of the things I have to remember.",
-        "I like to play around with ideas of my own even if they do not get me very far.",
-        "When I finish a piece of work, I check it through to see if it really meets the requirements.",
-        "Often I lie awake worrying about work I think I will not be able to do.",
-        "It is important for me to be able to follow the argument, or to see the reason behind things.",
-        "I do not find it at all difficult to motivate myself.",
-        "I like to be told precisely what to do in essays or other assignments.",
-        "I sometimes get hooked on academic topics and feel I would like to keep on studying them."
+        ["I manage to find conditions for studying which allow me to get on with my work easily.", "S", "O"],
+        ["When working on an assessment, I am keeping in mind how best to inpress the Instructor.", "S", "D"],
+        ["Often I find myself wondering whether the work I am doing here is really worthwhile.", "A", "L"],
+        ["I usually set out to understand for myself the meaning of what we have to learn.", "D", "S"],
+        ["I organize my study time carefully to make the best use of it.", "S", "T"],
+        ["I find i have to concentrate on just memorizing a good deal of what we have to learn.", "A", "U"],
+        ["I go over the work I have done carefullyto check the reasoning and that it makes sense.", "S", "M"],
+        ["Often I feel I am drowning in the sheer amount of material we are having to cope with.", "A", "F"],
+        ["I look at the evidence carefully and try to reach my own conclusion about what I am studying.", "D", "E"],
+        ["It is important for me to feel that I am doing as well as I really can on the courses here.", "S", "A"],
+        ["I try to relate ideas I come across to those in other topics or other courses whenever possible.", "D", "R"],
+        ["I tend to read very little beyond what is actually required to pass.", "A", "S"],
+        ["Regularly I find myself thinking about ideas from lectures when I am doing other things.", "D", "I"],
+        ["I think I am quite systematic and organised when it comes to revising for exams.", "S", "O"],
+        ["I look carefully at tutors comments on course work to see how to get higher marks next time.", "S", "D"],
+        ["There is not much of the work here that I find interesting or relevant.", "A", "L"],
+        ["When I read an article or book, I try to find out for myself exactly what the author means.", "D", "S"],
+        ["I am pretty good at getting down to work whenever I need to.", "S", "T"],
+        ["Much of what I am studying makes little sense: it is like unrelated bits and pieces.", "A", "U"],
+        ["I think about what I want to get out of this course to keep my studying well focused.", "S", "M"],
+        ["When I am working on a new topic, I try to see in my own mind how all the ideas fit together.", "D", "R"],
+        ["I often worry about whether I will ever be able to cope with the work properly.", "A", "F"],
+        ["Often I find myself questioning things I hear in lectures or read in books.", "D", "E"],
+        ["I feel that I am getting on well, and this helps me put more effort into the work.", "S", "A"],
+        ["I concentrate on learning just those bits of information I have to know to pass.", "A", "S"],
+        ["I find that studying academic topics can be quite exciting at times.", "D", "I"],
+        ["I am good at following up some of the reading suggested by lecturers or tutors.", "S", "O"],
+        ["I keep in mind who is going to mark an assignment and what they are likely to be looking for.", "S", "D"],
+        ["When I look back, I sometimes wonder why I ever decided to come here.", "A", "L"],
+        ["When I am reading, I stop from time to time to reflect on what I am trying to learn from it.", "D", "S"],
+        ["I work steadily through the term or semester, rather than leave it all until the last minute.", "S", "T"],
+        ["I am not really sure what is important in lectures so I try to get down all I can.", "A", "U"],
+        ["Ideas in course books or articles often set me off on long chains of thought of my own.", "D", "R"],
+        ["Before starting work on an assignment or exam question, I think first how best to tackle it.", "S", "M"],
+        ["I often seem to panic if I get behind with my work.", "A", "F"],
+        ["When I read, I examine the details carefully to see how they fit in with what is being said.", "D", "E"],
+        ["I put a lot of effort into studying because I am determined to do well.", "S", "A"],
+        ["I gear my studying closely to just what seems to be required for assignments and exams.", "A", "S"],
+        ["Some of the ideas I come across on the course I find really gripping.", "D", "I"],
+        ["I usually plan out my weeks work in advance, either on paper or in my head.", "S", "O"],
+        ["I keep an eye open for what lecturers seem to think is important and concentrate on that.", "S", "D"],
+        ["I am not really interested in this course, but I have to take it for other reasons.", "A", "L"],
+        ["Before tackling a problem or assignment, I first try to work out what lies behind it.", "D", "S"],
+        ["I generally make good use of my time during the day.", "S", "T"],
+        ["I often have trouble in making sense of the things I have to remember.", "A", "U"],
+        ["I like to play around with ideas of my own even if they do not get me very far.", "D", "R"],
+        ["When I finish a piece of work, I check it through to see if it really meets the requirements.", "S", "M"],
+        ["Often I lie awake worrying about work I think I will not be able to do.", "A", "F"],
+        ["It is important for me to be able to follow the argument, or to see the reason behind things.", "D", "E"],
+        ["I do not find it at all difficult to motivate myself.", "S", "A"],
+        ["I like to be told precisely what to do in essays or other assignments.", "A", "S"],
+        ["I sometimes get hooked on academic topics and feel I would like to keep on studying them.", "D", "I"]
         ]
         preferences =[
-        "lecturers who tell us exactly what to put down in our notes.",
-        "lecturers who encourage us to think for ourselves and show us how they themselves think.",
-        "exams which allow me to show that I have thought about the course material for myself.",
-        "exams or tests which need only the material provided in our lecture notes.",
-        "courses in which it is made very clear just which books we have to read.",
-        "courses where we are encouraged to read around the subject a lot for ourselves.",
-        "books which challenge you and provide explanations which go beyond the lectures.",
-        "books which give you definite facts and information which can easily be learned.",
+        ["lecturers who tell us exactly what to put down in our notes.", "P", "T"],
+        ["lecturers who encourage us to think for ourselves and show us how they themselves think.", "P", "S"],
+        ["exams which allow me to show that I have thought about the course material for myself.", "P", "S"],
+        ["exams or tests which need only the material provided in our lecture notes.", "P", "T"],
+        ["courses in which it is made very clear just which books we have to read.", "P", "T"],
+        ["courses where we are encouraged to read around the subject a lot for ourselves.", "P", "S"],
+        ["books which challenge you and provide explanations which go beyond the lectures.", "P", "S"],
+        ["books which give you definite facts and information which can easily be learned.", "P", "T"]
         ]
         assess = [
-        "How well do you think you have been doing in your assessed work overall, so far?"
+        ["How well do you think you have been doing in your assessed work overall, so far?"]
         ]
-
         i = 1
         for question in questions:
-            label = question
+            label = question[0]
             choices = [(1, 'Strongly Disagree'),(2,'Disagree'),(3,'Neither Agree Nor Disagree'),(4,'Agree'),(5,'Strongly Agree')]
             field = forms.ChoiceField(label=label, choices=choices,
-                    widget=forms.RadioSelect, required=True)
+                    widget=forms.RadioSelect(attrs={'score_category':question[1],'score_subcategory':question[2]}), required=True)
             self.fields['%d' % i] = field
             i += 1
         for question in preferences:
-            label = question
+            label = question[0]
             choices = [(1, 'Hate'),(2,'Dislike'),(3,'No Preference'),(4,'Like'),(5,'Love')]
             field = forms.ChoiceField(label=label, choices=choices,
-                    widget=forms.RadioSelect, required=True)
+                    widget=forms.RadioSelect(attrs={'score_category':question[1],'score_subcategory':question[2]}), required=True)
             self.fields['%d' % i] = field
             i += 1
         for question in assess:
-            label = question
+            label = question[0]
             choices = [(1, 'Badly'),(2,'Not So Well'),(3,'Average'),(4,'Well'),(5,'Excellent')]
             field = forms.ChoiceField(label=label, choices=choices,
                     widget=forms.RadioSelect, required=True)
--- studyskills/views.py
+++ studyskills/views.py
@@ -27,9 +27,23 @@ def study_skills_show_assessment(request):
            answers = []
            for item in form.cleaned_data:
                if item in form.fields:
+                   # Retrieve the scoring category and sub-category from
+                   # field attributes for this field and save them with
+                   # the answer as they'll be needed for scoring purposes.
+                   field_attrs = form.fields[item].widget.attrs
+                   if 'score_category' in field_attrs:
+                       score_category = field_attrs["score_category"]
+                   else:
+                       score_category = ""
+                   if 'score_subcategory' in field_attrs:
+                       score_subcategory = field_attrs["score_subcategory"]
+                   else:
+                       score_subcategory = ""
                    answers.append({'question': form.fields[item].label,
                                    'answer': form.cleaned_data[item],
-                                   'question_id': item})
+                                   'question_id': item,
+                                   'score_category': score_category,
+                                   'score_subcategory': score_subcategory})
            answers = json.dumps(answers)
            result = models.StudySkillsResult(
                student = request.user,
@@ -82,6 +96,70 @@ def study_skills_get_result(request, result_id):
         raise PermissionDenied
 
     answers = json.loads(result.answers)
-    #TODO: add scoring
 
-    return direct_to_template(request, 'studyskills/study-skills-result.html', {})
+    # Create dictionaries for storing totals that will be used to
+    # calculate scores and subscores
+    scores = {'D': {'total': 0, 'count_total': 0},
+              'S': {'total': 0, 'count_total': 0},
+              'A': {'total': 0, 'count_total': 0},
+              'P': {'total': 0, 'count_total': 0}
+             } 
+    subscores = {'DS': {'total': 0, 'count_total': 0},
+                 'DR': {'total': 0, 'count_total': 0},
+                 'DE': {'total': 0, 'count_total': 0},
+                 'DI': {'total': 0, 'count_total': 0},
+                 'SO': {'total': 0, 'count_total': 0},
+                 'ST': {'total': 0, 'count_total': 0},
+                 'SD': {'total': 0, 'count_total': 0},
+                 'SA': {'total': 0, 'count_total': 0},
+                 'SM': {'total': 0, 'count_total': 0},
+                 'AL': {'total': 0, 'count_total': 0},
+                 'AU': {'total': 0, 'count_total': 0},
+                 'AS': {'total': 0, 'count_total': 0},
+                 'AF': {'total': 0, 'count_total': 0},
+                 'PS': {'total': 0, 'count_total': 0},
+                 'PT': {'total': 0, 'count_total': 0}
+                }
+
+    # Process each of the student's answers
+    for answer in answers:
+        if 'score_category' in answer and 'score_subcategory' in answer:
+            if answer['score_category'] != '' and answer['score_subcategory'] != '':
+                subscores[answer['score_category']+answer['score_subcategory']]['total']+=float(answer['answer'])
+                subscores[answer['score_category']+answer['score_subcategory']]['count_total']+=15
+            
+                scores[answer['score_category']]['total']+=float(answer['answer'])
+                scores[answer['score_category']]['count_total']+=15    
+
+    # Compile the information and calculate the actual scores
+    compiled_scores = [{'category':'Deep approach', 'score': scores['D']['total'] / scores['D']['count_total'] * 100,
+                                  'sub_cats': [{'sub_cat': 'Seeking meaning', 'sub_cat_score': subscores['DS']['total'] / subscores['DS']['count_total'] * 100},
+                                               {'sub_cat': 'Relating ideas', 'sub_cat_score': subscores['DR']['total'] / subscores['DR']['count_total'] * 100},
+                                               {'sub_cat': 'Use of evidence', 'sub_cat_score': subscores['DE']['total'] / subscores['DE']['count_total'] * 100},
+                                               {'sub_cat': 'Interest in ideas', 'sub_cat_score': subscores['DI']['total'] / subscores['DI']['count_total'] * 100}
+                                              ]
+                           },
+                           {'category':'Strategic approach', 'score': scores['S']['total'] / scores['S']['count_total'] * 100,
+                                  'sub_cats': [{'sub_cat': 'Organized studying', 'sub_cat_score': subscores['SO']['total'] / subscores['SO']['count_total'] * 100},
+                                               {'sub_cat': 'Time management', 'sub_cat_score': subscores['ST']['total'] / subscores['ST']['count_total'] * 100},
+                                               {'sub_cat': 'Alertness to assessment demands', 'sub_cat_score': subscores['SD']['total'] / subscores['SD']['count_total'] * 100},
+                                               {'sub_cat': 'Achieving', 'sub_cat_score': subscores['SA']['total'] / subscores['SA']['count_total'] * 100},
+                                               {'sub_cat': 'Monitoring effectiveness', 'sub_cat_score': subscores['SM']['total'] / subscores['SM']['count_total'] * 100}
+                                              ]
+                           },
+                           {'category':'Surface apathetic approach', 'score': scores['A']['total'] / scores['A']['count_total'] * 100,
+                                  'sub_cats': [{'sub_cat': 'Lack of purpose', 'sub_cat_score': subscores['AL']['total'] / subscores['AL']['count_total'] * 100},
+                                               {'sub_cat': 'Unrelated memorizing', 'sub_cat_score': subscores['AU']['total'] / subscores['AU']['count_total'] * 100},
+                                               {'sub_cat': 'Syllabus-boundness', 'sub_cat_score': subscores['AS']['total'] / subscores['AS']['count_total'] * 100},
+                                               {'sub_cat': 'Fear of failure', 'sub_cat_score': subscores['AF']['total'] / subscores['AF']['count_total'] * 100}
+                                              ]
+                           },
+                           {'category':'Preferences for different types of course and teaching', 'score': scores['P']['total'] / scores['P']['count_total'] * 100,
+                                  'sub_cats': [{'sub_cat': 'Supporting understanding', 'sub_cat_score': subscores['PS']['total'] / subscores['PS']['count_total'] * 100},
+                                               {'sub_cat': 'Transmitting information', 'sub_cat_score': subscores['PT']['total'] / subscores['PT']['count_total'] * 100}
+                                              ]
+                           }
+                      ]
+
+    # Display the results
+    return direct_to_template(request, 'studyskills/study-skills-result.html', {'scores': compiled_scores})
